720a303dc7d49254041f17f4070663095151d51e
Saeed <dotnettashkent@gmail.com>
2023-12-12 19:22:56 +0500

    [Feat] - implement fusion server

---
+++ Server/FusionServerExtension.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Features.Banner;
+using Stl.Fusion;
+
+namespace Server
+{
+    public static class FusionServerExtension
+    {
+        public static FusionBuilder AddEbazarServices(this FusionBuilder fusion)
+        {
+            fusion.AddService<IBannerService>();
+//            fusion.AddService<IFileService>();
+            return fusion;
+        }
+    }
+}
