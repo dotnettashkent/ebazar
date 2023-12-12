158124f0bab6fe0fe160024b677c623f23736bd2
Saeed <dotnettashkent@gmail.com>
2023-12-12 16:32:32 +0500

    [Feat] - added Product interface and others

---
+++ Shared/Features/Product/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Infrastructures;
+using Stl.Async;
+using Stl.Fusion;
+using System.Reactive;
+
+namespace Shared.Features.Product
+{
+    public interface IProductService : IComputeService
+    {
+        [ComputeMethod]
+        Task<List<ProductView>> GetAll(TableOptions options , CancellationToken cancellationToken = default);
+        [ComputeMethod]
+        Task<ProductView> GetById(long id, CancellationToken cancellationToken = default);
+        Task Create(CreateProductCommand command, CancellationToken cancellationToken = default);
+        Task Delete(DeleteProductCommand command, CancellationToken cancellationToken = default);
+        Task Update(UpdateProductCommand command , CancellationToken cancellationToken = default);
+        Task<Unit> Invalidate() { return TaskExt.UnitTask; }
+    }
+}
---
+++ Shared/Infrastructures/Extensions/TableOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Infrastructures
+{
+    public class TableOptions
+    {
+    }
+}
