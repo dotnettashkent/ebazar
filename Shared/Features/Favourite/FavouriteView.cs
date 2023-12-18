9b150de92430b358c8dd7674930ca41bff08d9b2
Saeed <dotnettashkent@gmail.com>
2023-12-18 18:18:50 +0500

    [Feat] - create Favourite classes

---
+++ Shared/Features/Favourite/FavouriteView.cs
@@ -0,0 +1,15 @@
+﻿using MemoryPack;
+using Stl.Fusion.Blazor;
+using System.Runtime.Serialization;
+namespace Shared.Features
+{
+
+	[DataContract, MemoryPackable]
+	[ParameterComparer(typeof(ByValueParameterComparer))]
+	public partial class FavouriteView
+	{
+		[property : DataMember]	public long Id { get; set; }
+		[property : DataMember]	public long UserId { get; set; }
+		[property : DataMember]	public long ProductId { get; set; }
+	}
+}
