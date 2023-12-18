733158fab912c92e110f7556922ceed75a898853
Saeed <dotnettashkent@gmail.com>
2023-12-18 16:40:35 +0500

    [Fix] - implement order interface and class

---
+++ Shared/Features/Order/OrderCommands.cs
@@ -0,0 +1,16 @@
+﻿using MemoryPack;
+using Stl.Fusion;
+using System.Runtime.Serialization;
+
+namespace Shared.Features
+{
+	[DataContract, MemoryPackable]
+	public partial record CreateOrderCommand([property: DataMember] Session Session, [property: DataMember] OrderView Entity) : ISessionCommand<OrderView>;
+
+	[DataContract, MemoryPackable]
+	public partial record UpdateOrderCommand([property: DataMember] Session Session, [property: DataMember] OrderView Entity) : ISessionCommand<OrderView>;
+
+	[DataContract, MemoryPackable]
+	public partial record DeleteOrderCommand([property: DataMember] Session Session, [property: DataMember] int Id) : ISessionCommand<OrderView>;
+
+}
---
+++ Shared/Features/Order/OrderView.cs
@@ -0,0 +1,19 @@
+﻿using MemoryPack;
+using Stl.Fusion.Blazor;
+using System.Runtime.Serialization;
+
+namespace Shared.Features
+{
+	[DataContract, MemoryPackable]
+	[ParameterComparer(typeof(ByValueParameterComparer))]
+	public partial class OrderView
+	{
+		[property : DataMember] public long Id { get; set; }
+		[property : DataMember] public long CartId { get; set; }
+		[property : DataMember] public long UserId { get; set; }
+		[property : DataMember] public long CourierId { get; set; }
+		[property : DataMember] public bool IsSuccess { get; set; } = false;
+		[property : DataMember] public string UserComment { get; set; } = string.Empty;
+
+	}
+}
