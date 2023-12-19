6960440e06e7a95524dd766846b07f21c69947e7
Saeed <dotnettashkent@gmail.com>
2023-12-19 15:03:47 +0500

    [Feat] - create brand classes and interface

---
+++ Shared/Features/Brands/BrandEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Features.Brands
+{
+	public class BrandEntity
+	{
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[Column("id")]
+		public long Id { get; set; }
+
+		[Column("name")]
+		public string Name { get; set; } = null!;
+
+		[Column("is_popular")]
+		public string IsPopular { get; set; } = null!;
+
+		[Column("photo_id")]
+		public long PhotoId { get; set; }
+	}
+}
---
+++ Shared/Features/Brands/IBrandService.cs
@@ -0,0 +1,29 @@
+﻿using Shared.Infrastructures.Extensions;
+using Shared.Infrastructures;
+using Stl.Async;
+using Stl.CommandR.Configuration;
+using Stl.Fusion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Features.Brands
+{
+	public interface IBrandService : IComputeService
+	{
+		//[ComputeMethod]
+		Task<TableResponse<BrandView>> GetAll(TableOptions options, CancellationToken cancellationToken = default);
+		//[ComputeMethod]
+		Task<BrandView> Get(long id, CancellationToken cancellationToken = default);
+		[CommandHandler]
+		Task Create(CreateBannerCommand command, CancellationToken cancellationToken = default);
+		[CommandHandler]
+		Task Update(UpdateBannerCommand command, CancellationToken cancellationToken = default);
+		[CommandHandler]
+		Task Delete(DeleteBannerCommand command, CancellationToken cancellationToken = default);
+		Task<Unit> Invalidate() { return TaskExt.UnitTask; }
+	}
+}
