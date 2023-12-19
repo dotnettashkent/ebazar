926db931c15db9b99bb6f87507531f0223e36bd1
Saeed <dotnettashkent@gmail.com>
2023-12-19 17:35:36 +0500

    [Feat] - implement AppDbContext

---
+++ Service/Data/PostgresContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Features;
+using Shared.Features.Brands;
+using Shared.Features.Favourite;
+using Shared.Features.File;
+
+namespace Service.Data
+{
+	public partial class PostgresContext
+	{
+		public virtual DbSet<AddressEntity> Addresses { get; set; }
+		public virtual DbSet<BannerEntity> Banners { get; set; }
+		public virtual DbSet<BrandEntity> Brands { get; set; }
+		public virtual DbSet<CartEntity> Carts { get; set; }
+		public virtual DbSet<CourierEntity> Couriers { get; set; }
+		public virtual DbSet<FavouriteEntity> Favorites { get; set; }
+		public virtual DbSet<FileEntity> Files { get; set; }
+		public virtual DbSet<OrderEntity> Orders { get; set; }
+		public virtual DbSet<ProductEntity> Products { get; set; }
+		public virtual DbSet<ProductCategoryEntity> ProductCategories { get; set; }
+		public virtual DbSet<UserEntity> Users { get; set; }
+	}
+}
---
+++ Service/Features/ProductCategory/ProductCategoryService.cs
@@ -0,0 +1,60 @@
+﻿using Shared.Infrastructures.Extensions;
+using Shared.Infrastructures;
+using Shared.Features;
+using Stl.Async;
+using Stl.Fusion;
+using System.Reactive;
+using Stl.Fusion.EntityFramework;
+using Service.Data;
+
+namespace Service.Features
+{
+	public class ProductCategoryService : IProductCategoryService
+	{
+		#region Initialize
+		private readonly DbHub<AppDbContext> dbHub;
+
+		public ProductCategoryService(DbHub<AppDbContext> dbHub)
+		{
+			this.dbHub = dbHub;
+		}
+		#endregion
+
+		#region Queries
+		public async virtual Task<TableResponse<ProductCategoryView>> GetAll(TableOptions options, CancellationToken cancellationToken = default)
+		{
+			await Invalidate();
+
+			var dbContext = dbHub.CreateDbContext();
+			await using var _ = dbContext.ConfigureAwait(false);
+			var categories = from s in dbContext.
+		}
+
+		public Task<ProductCategoryView> GetById(long id, CancellationToken cancellationToken = default)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+		#region Mutations
+		public Task Create(CreateProductCategoryCommand command, CancellationToken cancellationToken = default)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task Delete(DeleteProductCategoryCommand command, CancellationToken cancellationToken = default)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task Update(UpdateProductCategoryCommand command, CancellationToken cancellationToken = default)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+		#region Helpers
+
+		[ComputeMethod]
+		public virtual Task<Unit> Invalidate() => TaskExt.UnitTask;
+		#endregion
+	}
+}
