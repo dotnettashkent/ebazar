0e2befe1c7ed52924c96417ba4847c2247b5e958
Saeed <dotnettashkent@gmail.com>
2023-12-20 15:04:46 +0500

    [Fix] - update fusion server

--- Server/FusionServerExtension.cs
+++ Server/FusionServerExtension.cs
@@ -1,15 +1,17 @@
-﻿using Shared.Features.Banner;
+﻿using Service.Features;
+using Service.Features.File;
+using Shared.Features;
 using Stl.Fusion;
 
 namespace Server
 {
     public static class FusionServerExtension
     {
-        /*public static FusionBuilder AddEbazarServices(this FusionBuilder fusion)
+        public static FusionBuilder AddEbazarServices(this FusionBuilder fusion)
         {
-            fusion.AddService<IBannerService>();
-//            fusion.AddService<IFileService>();
+            fusion.AddService<IProductCategoryService, ProductCategoryService>();
+            fusion.AddService<IFileService, FileService>();
             return fusion;
-        }*/
+        }
     }
 }
