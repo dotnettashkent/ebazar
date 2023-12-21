df21e0b0b85eb3cae0cb19a817019eb7c63de299
Saeed <dotnettashkent@gmail.com>
2023-12-21 19:24:45 +0500

    [Fix] - ProductCategory class update for locale

--- Shared/Features/ProductCategory/ProductCategoryEntity.cs
+++ Shared/Features/ProductCategory/ProductCategoryEntity.cs
@@ -10,56 +10,26 @@ namespace Shared.Features
 		[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public long Id { get; set; }
 
-		[Column("main_name_uz")]
-		public string MainNameUz { get; set; } = null!;
+		[Column("locale")]
+		public string Locale { get; set; } = null!;
 
-		[Column("main_name_ru")]
-		public string MainNameRu { get; set; } = null!;
+		[Column("main_name")]
+		public string MainName { get; set; } = null!;
 
-		[Column("main_name_en")]
-		public string MainNameEn { get; set; } = null!;
+		[Column ("main_link")]
+		public string MainLink { get; set;} = null!;
 
-		[Column ("main_link_uz")]
-		public string MainLinkUz { get; set;} = null!;
+		[Column("second_name")]
+		public string SecondName { get; set; } = null!;
 
-		[Column("main_link_ru")]
-		public string MainLinkRu { get; set; } = null!;
+		[Column("second_link")]
+		public string SecondLink { get; set; } = null!;
 
-		[Column("main_link_en")]
-		public string MainLinkEn { get; set; } = null!;
+		[Column("third_name")]
+		public string ThirdName { get; set;} = null!;
 
-		[Column("second_name_uz")]
-		public string SecondNameUz { get; set; } = null!;
-
-		[Column("second_name_ru")]
-		public string SecondNameRu { get; set; } = null!;
-
-		[Column("second_name_en")]
-		public string SecondNameEn { get; set; } = null!;
-
-		[Column("second_link_uz")]
-		public string SecondLinkUz { get; set; } = null!;
-
-		[Column("second_link_ru")]
-		public string SecondLinkRu { get; set; } = null!;
-
-		[Column("second_link_en")]
-		public string SecondLinkEn { get; set; } = null!;
-
-		[Column("third_name_uz")]
-		public string ThirdNameUz { get; set;} = null!;
-
-		[Column("third_name_ru")]
-		public string ThirdNameRu { get; set; } = null!;
-
-		[Column("third_name_en")]
-		public string ThirdNameEn { get; set; } = null!;
-
-		[Column("third_link_uz")]
-		public string ThirdLinkUz { get; set;} = null!;
-
-		[Column("third_link_ru")]
-		public string ThirdLinkRu { get; set; } = null!;
+		[Column("third_link")]
+		public string ThirdLink { get; set;} = null!;
 
 		[Column("is_popular")]
 		public bool IsPopular { get; set; }
