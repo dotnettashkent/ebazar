1ddf412c5dff3b13f11bacd1591d7211a6fe4f2d
Saeed <dotnettashkent@gmail.com>
2024-01-08 15:00:10 +0500

    [Feat] - unknown big changes

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,11 +3,6 @@ using Service.Data;
 using EF.Audit.Core;
 using Microsoft.EntityFrameworkCore;
 using Server.Infrastructure.ServiceCollection;
-using MudBlazor.Services;
-using MudBlazor;
-using Shared.Infrastructures;
-using Client.Core.Services;
-using Blazored.LocalStorage;
 
 #region Builder
 var builder = WebApplication.CreateBuilder(args);
@@ -23,8 +18,8 @@ services.AddDataBase<AppDbContext>(env, cfg, (DataBaseType)Enum.Parse(typeof(Dat
 // Register IDbContextFactory<AuditDbContext> before AddDataBase<AppDbContext>
 services.AddDbContextFactory<AuditDbContext>(options =>
 {
-	// Configure options for AuditDbContext
-	options.UseNpgsql(cfg.GetConnectionString("Default"));
+    // Configure options for AuditDbContext
+    options.UseNpgsql(cfg.GetConnectionString("Default"));
 });
 #endregion
 
@@ -36,42 +31,20 @@ builder.Services.AddSwaggerDocument();
 IComputedState.DefaultOptions.MustFlowExecutionContext = true;
 builder.Services.AddFusionServices();
 #endregion
-#region MudBlazor and Pages
-services.AddRazorPages();
-services.AddServerSideBlazor(o => o.DetailedErrors = true);
-services.AddMudServices(config =>
-{
-    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
-    config.SnackbarConfiguration.PreventDuplicates = false;
-    config.SnackbarConfiguration.NewestOnTop = false;
-    config.SnackbarConfiguration.ShowCloseIcon = true;
-    config.SnackbarConfiguration.VisibleStateDuration = 10000;
-    config.SnackbarConfiguration.HideTransitionDuration = 500;
-    config.SnackbarConfiguration.ShowTransitionDuration = 500;
-    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
-});
-#endregion
-#region UI Services
-services.AddBlazoredLocalStorage();
-services.AddScoped<IUserPreferencesService, UserPreferencesService>();
-services.AddScoped<LayoutService>();
-services.AddSingleton<UserContext>();
-services.AddScoped<PageHistoryState>();
-#endregion
-services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseDeveloperExceptionPage();
-	app.UseOpenApi();
-	app.UseSwaggerUi3();
+    app.UseDeveloperExceptionPage();
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
 }
 else
 {
-	app.UseExceptionHandler("/Error");
-	app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
