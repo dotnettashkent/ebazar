cfaebbc42389fbb51eca988d01d46cacb2173aad
Saeed <dotnettashkent@gmail.com>
2024-01-11 10:41:51 +0500

    [Feat] - implement api controller for Cart

--- Server/Controllers/Cart/CartController.cs
+++ Server/Controllers/Cart/CartController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Service.Features;
 using Shared.Features;
+using Shared.Infrastructures.Extensions;
 using Stl.CommandR;
 
 namespace Server.Controllers
@@ -21,5 +23,17 @@ namespace Server.Controllers
 		{
 			return commander.Call(command, cancellationToken);
 		}
-	}
+
+		[HttpDelete()]
+		public Task Delete([FromBody] DeleteCartCommand command, CancellationToken cancellationToken)
+		{
+            return commander.Call(command, cancellationToken);
+        }
+
+        [HttpGet("get/favourites")]
+        public Task<TableResponse<ProductView>> GetAll(long userId)
+        {
+            return cartService.GetAll(userId);
+        }
+    }
 }
