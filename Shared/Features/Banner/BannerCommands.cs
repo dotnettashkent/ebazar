aa777afbc7bc66fc26ccb7fcd59dc9d5c6f887c6
Saeed <dotnettashkent@gmail.com>
2024-02-16 21:00:38 +0500

    [Feat] - implement banner service with token based

--- Shared/Features/Banner/BannerCommands.cs
+++ Shared/Features/Banner/BannerCommands.cs
@@ -5,12 +5,21 @@ using System.Runtime.Serialization;
 namespace Shared.Features
 {
     [DataContract, MemoryPackable]
-    public partial record CreateBannerCommand([property: DataMember] Session Session, [property: DataMember] BannerView Entity) : ISessionCommand<BannerView>;
+    public partial record CreateBannerCommand(
+        [property: DataMember] Session Session, 
+        [property: DataMember] BannerView Entity,
+        [property: DataMember] string Token) : ISessionCommand<BannerView>;
 
     [DataContract, MemoryPackable]
-    public partial record UpdateBannerCommand([property: DataMember] Session Session, [property: DataMember] BannerView Entity) : ISessionCommand<BannerView>;
+    public partial record UpdateBannerCommand(
+        [property: DataMember] Session Session, 
+        [property: DataMember] BannerView Entity,
+        [property: DataMember] string Token) : ISessionCommand<BannerView>;
 
     [DataContract, MemoryPackable]
-    public partial record DeleteBannerCommand([property: DataMember] Session Session, [property: DataMember] long Id) : ISessionCommand<BannerView>;
+    public partial record DeleteBannerCommand(
+        [property: DataMember] Session Session, 
+        [property: DataMember] long Id,
+        [property: DataMember] string Token) : ISessionCommand<BannerView>;
 
 }
