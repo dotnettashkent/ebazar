dfdfc0a3ac47b40da7dcd54367bd56b331863a16
Saeed <dotnettashkent@gmail.com>
2024-03-29 16:00:01 +0500

    [Fix] - remove www string value

--- Shared/Features/Brands/BrandView.cs
+++ Shared/Features/Brands/BrandView.cs
@@ -1,6 +1,7 @@
 ﻿using MemoryPack;
 using Microsoft.AspNetCore.Http;
 using Stl.Fusion.Blazor;
+using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
@@ -23,7 +24,25 @@ namespace Shared.Features
         [JsonIgnore]
         public IFormFile? Photo { get; set; }
 
-        [property: DataMember][JsonPropertyName("brand_image")] public string? ImageOne { get; set; }
+        private string? _imageOne;
 
+        [property: DataMember]
+        [JsonPropertyName("brand_image")]
+        public string? ImageOne
+        {
+            get => _imageOne;
+            set
+            {
+                if (value != null)
+                {
+                    // Remove "www" from the URL
+                    _imageOne = value.Replace("www.", "");
+                }
+                else
+                {
+                    _imageOne = null;
+                }
+            }
+        }
     }
 }
